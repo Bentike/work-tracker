5fe0d3e05e3917f8846f7975ec49e99c8cf511e9
bentike <ajadiyusuf73@gmail.com>
2024-01-31 14:30:31 +0100

    added signup header text

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,7 +1,8 @@
 const SignUp = () => {
     return(
-        <div className="shadow-md text-center bg-white w-1/2 mx-auto mt-5 py-10">
-            <h3 className="text-black">Sign Up</h3>
+        <div className="text-black shadow-md text-center bg-white w-1/2 mx-auto mt-5 py-10">
+            <h3 className="font-bold text-2xl mb-1">Sign Up</h3>
+            <p>Enter your details below so we can get you started</p>
         </div>
     );
 }
